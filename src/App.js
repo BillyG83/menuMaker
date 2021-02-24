7ac2441b5e8dfc6d652a832c8a0261dad97ccb03
BillyG83 <bgilmore83@hotmail.co.uk>
2021-02-24 17:17:26 +0100

    Revert "chore(gitignore): ds store file"
    This reverts commit afd01fcb630bfaf5196e3551b148a8b0b84ea1a3.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import React from 'react'
-import { Route } from 'react-router-dom'
 import './App.css';
-import LandingPage from './pages/landing/landing.component.jsx'
-import AdminPage from './pages/admin/admin.component.jsx'
 
 function App() {
-	return (
-		<div className="app">
-			<Route 
-				exact={true} 
-				path='/' 
-				component={LandingPage}
-			/>
-
-			<Route 
-				exact={true} 
-				path='/admin' 
-				component={AdminPage}
-			/>
-		</div>
-	)
+  return (
+    <div>
+      <p>lets go!</p>
+    </div>
+  );
 }
 
 export default App;
