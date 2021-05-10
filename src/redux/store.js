86e1805cd5c1b2aa8699c8fe1013058780590f4d
BillyG83 <bgilmore83@hotmail.co.uk>
2021-05-10 18:24:09 +0200

    chore(store): hide middleware from production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,10 @@ import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import rootReducer from './root-reducer.js'
 
-const middleWears = [logger]
+const middleWears = []
+if (process.env.NODE_ENV === 'development') {
+    middleWears.push(logger)
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middleWears))
 
