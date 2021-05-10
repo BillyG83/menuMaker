647d4a82f5c8cbfc35ada41fd259c35757210113
BillyG83 <bgilmore83@hotmail.co.uk>
2021-05-10 18:25:20 +0200

    feat(checkout page): added for subscription stripe payment

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import StripeCheckout from '../../components/stripe-button/stripe-button.component.jsx'
 import './checkout.styles.scss'
 
 const CheckoutPage = () => (
     <div className="checkout">
-        <h1>Checkout</h1>
-        <p>this will be the checkout page</p>
+        <h1>Upgrade to pro version</h1>
+        <p>this will give you:</p>
+        <ol>
+            <li>No ads on your menu</li>
+            <li>Add images of your menu items</li>
+            <li>Add more than one menu to your account</li>
+            <li>Access to future features currently in development</li>
+        </ol>
+        <br />
+        <strong>Test payment: 4242 4242 4242 4242 <br /> EXP: 01/22 - CVV: 123</strong>
+        <br />
+        <StripeCheckout price={5} />
+        <br />
+        <br />
     </div>
 )
 
