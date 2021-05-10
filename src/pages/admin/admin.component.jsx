541b0d4109195434293b85a34194956ce04be573
BillyG83 <bgilmore83@hotmail.co.uk>
2021-05-10 18:26:39 +0200

    feat(admin): refactoring code and using new components

--- src/pages/admin/admin.component.jsx
+++ src/pages/admin/admin.component.jsx
@@ -1,52 +1,24 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import { selectCurrentUser } from '../../redux/user/user.selectors.js'
 import { signInWithGoogle } from '../../firebase/firebase.js'
-import { auth } from '../../firebase/firebase.js'
 
-import SignIn from '../../components/sign-in/sign-in.component.jsx'
-import StripeCheckout from '../../components/stripe-button/stripe-button.component.jsx'
+import SignedInUserAdmin from '../../components/admin-logged-in/admin-logged-in.component.jsx'
 
 import './admin.styles.scss'
 
-class AdminPage extends React.Component {
-	render () {
-		return(
-			<div className="admin-page">
-				<h1>Admin Page</h1>
-				<p>user can add, edit, remove products</p>
-				<br />
-				<Link to='/'>Back to menu</Link>
-				<br /><br />
-				<SignIn />
-				<br />
-				<br />
-				<h2>Upgrade to pro version</h2>
-				<p>this will give you:</p>
-				<ol>
-					<li>No ads on your menu</li>
-					<li>Add images of your menu items</li>
-					<li>Add more than one menu to your account</li>
-					<li>Access to future features currently in development</li>
-				</ol>
-				<br />
-				<strong>Test payment: 4242 4242 4242 4242 <br /> EXP: 01/22 - CVV: 123</strong>
-				<br />
-				<StripeCheckout price={5} />
-				<br />
-				<br />
-				{
-					this.props.currentUser ?
-					<button onClick={() => auth.signOut()}>Sign out</button>
-					:
-					<button onClick={signInWithGoogle}>Sign in with Google</button>
-				}
-			</div>
-		)
-	}
-}
+const AdminPage = ({currentUser}) => (
+	<section className="admin-page">
+		<h1>My Account</h1>
+		{
+			currentUser ?
+			<SignedInUserAdmin currentUser={currentUser} />
+			:
+			<button onClick={signInWithGoogle}>Sign in with Google</button>
+		}		
+	</section>
+)
 
 const mapStateToProps = state => ({
 	currentUser: selectCurrentUser(state)
