79c991eb1b53433af7fc9141ccdfba922f28008b
BillyG83 <bgilmore83@hotmail.co.uk>
2021-05-13 16:39:14 +0200

    feat(admin-logged-in): connecting to store and database to get and set users accounts

--- src/components/admin-logged-in/admin-logged-in.component.jsx
+++ src/components/admin-logged-in/admin-logged-in.component.jsx
@@ -1,23 +1,40 @@
 import React from 'react'
-import { auth } from '../../firebase/firebase.js'
+import { connect } from 'react-redux'
+import { selectCurrentUser } from '../../redux/user/user.selectors.js'
+import { setUsersAccounts } from '../../redux/user/user.actions.js'
+import { auth , getUsersMenus } from '../../firebase/firebase.js'
 import AddMenu from '../add-menu/add-menu.component.jsx'
 
-const SignedInUserAdmin = ({ currentUser }) => (
-    <section className="user-signed-in">
-        <h2>{`Welcome ${currentUser.displayName}`}</h2>
+const SignedInUserAdmin = ({ currentUser, setUsersAccounts }) => {
+    const userAccounts = getUsersMenus(currentUser.id)
+    userAccounts.then((value) => {
+        setUsersAccounts(value)
+    });
+    return(
+        <section className="user-signed-in">
+            <h2>{`Welcome ${currentUser.displayName}`}</h2>
 
-        <div className="admin-add-menu">
-            {
-                currentUser.accounts ?
-                <h3>Add a new menu</h3>
-                :
-                <h3>Create your menu</h3>
-            }
-            <AddMenu userId={currentUser.id} />
-        </div>
-        
-        <button onClick={() => auth.signOut()}>Sign out</button>
-    </section>
-)
+            <div className="admin-add-menu">
+                {
+                    currentUser.accounts ?
+                    <h3>Add a new menu</h3>
+                    :
+                    <h3>Create your menu</h3>
+                }
+                <AddMenu userId={currentUser.id} />
+            </div>
+            
+            <button onClick={() => auth.signOut()}>Sign out</button>
+        </section>
+    )
+}
 
-export default SignedInUserAdmin
+const mapStateToProps = state => ({
+	currentUser: selectCurrentUser(state)
+})
+
+const mapDispatchToProps = dispatch => ({
+    setUsersAccounts: (data) => dispatch(setUsersAccounts(data))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignedInUserAdmin)
