4f2b0369bd2cf74e5f541cff9a16e6e7a8ae3939
BillyG83 <bgilmore83@hotmail.co.uk>
2021-05-16 17:34:06 +0200

    feat(account-list): updated UI

--- src/components/account-list/account-list.component.jsx
+++ src/components/account-list/account-list.component.jsx
@@ -1,21 +1,30 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { selectUserAccounts } from '../../redux/accounts/accounts.selectors.js'
+import { userSelectedAccount } from '../../firebase/firebase.js'
+import './account-list.styles.scss'
 
-const AccountList = ({ menus }) => (
-    <div>
-        <ul>
-            {
-                menus && menus.map(menu => (
-                    <li key={menu.businessId}>{menu.businessName}</li>
-                ))
-            }
-        </ul>
-    </div>
+const AccountList = ({ accounts }) => (
+    <ul className="account-list">
+        {
+            accounts && accounts.map(account => (
+                <li key={account.businessId}>
+                    <button 
+                        id={account.businessId}
+                        onClick={userSelectedAccount}
+                    >
+                        <span>{account.businessName}</span>
+                        <span>{account.businessPostCode}</span>
+                        <span>{account.currency}</span>
+                    </button>
+                </li>
+            ))
+        }
+    </ul>
 )
 
 const mapStateToProps = state => ({
-	menus: selectUserAccounts(state),
+	accounts: selectUserAccounts(state),
 })
 
 export default connect(mapStateToProps)(AccountList)
