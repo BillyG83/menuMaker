d08da34d796cc2f125c5b6fb0abf312556d5802c
BillyG83 <bgilmore83@hotmail.co.uk>
2021-05-17 21:07:13 +0200

    feat(account-list): removed markup and styles, move to AccountCard

--- src/components/account-list/account-list.component.jsx
+++ src/components/account-list/account-list.component.jsx
@@ -1,25 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { selectUserAccounts } from '../../redux/accounts/accounts.selectors.js'
-import { userSelectedAccount } from '../../firebase/firebase.js'
+import AccountCard from '../account-card/account-card.component.jsx'
 import './account-list.styles.scss'
 
 const AccountList = ({ accounts }) => (
-    <ul className="account-list">
-        {
-            accounts && accounts.map(account => (
-                <li key={account.businessId}>
-                    <button 
-                        id={account.businessId}
-                        onClick={userSelectedAccount}
-                    >
-                        <span>{account.businessName}</span>
-                        <span>{account.businessPostCode}</span>
-                    </button>
-                </li>
-            ))
-        }
-    </ul>
+    <div className="account-list">
+        <p>
+            {
+            accounts ? 
+                'Here are your accounts:'
+            : 
+                'Hmmmm, your menus should show here'
+            }
+        </p>
+        <ul className="account-list">
+            {
+                accounts && accounts.map(account => (
+                    <li key={account.businessId}>
+                        <AccountCard account={account} />
+                    </li>
+                ))
+            }
+        </ul>
+    </div>
 )
 
 const mapStateToProps = state => ({
