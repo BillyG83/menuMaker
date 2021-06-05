2003b11b9e81f4c2416eedd5d91fc2cbc5ba59fa
BillyG83 <bgilmore83@hotmail.co.uk>
2021-06-05 15:21:34 +0200

    feat(sign-in): converted to hooks

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,63 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../button/button.component.jsx'
 import './sign-in.styles.scss'
 
-class SignIn extends React.Component {
-    constructor() {
-        super()
+const SignIn = () => {
+    const [userData, setUserData] = useState({ email: '', password: ''})
+    const { email, password } = userData
 
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
-
-    handleSubmit = event => {
+    const handleSubmit = event => {
         event.preventDefault()
-        this.setState({ email: '', password: '' })
+        console.log(email);
+        console.log(password);
+        setUserData({ email: '', password: '' })
     }
 
-    handleChange = event => {
+    const handleChange = event => {
         const { value, name } = event.target
-        this.setState({ [name] : value })
+        setUserData({ [name] : value })
     }
 
-    render() {
-        return (
-            <form className="sign-in form" onSubmit={this.handleSubmit}>
-                <label>
-                    email
-                    <input 
-                        name="email"
-                        onChange={this.handleChange}
-                        placeholder="your email" 
-                        required
-                        type="email" 
-                        value={this.state.email}
-                    />
-                </label>
-                
-                <label>
-                    password
-                    <input 
-                        name="password"
-                        onChange={this.handleChange}
-                        placeholder="your password" 
-                        required
-                        type="password" 
-                        value={this.state.password}
-                    />
-                </label>
-
-                <Button 
-                    id="submit-sign-in" 
-                    text="Submit"
-                    type="submit"
+    return (
+        <form className="sign-in form" onSubmit={handleSubmit}>
+            <label>
+                email
+                <input 
+                    name="email"
+                    onChange={handleChange}
+                    placeholder="your email" 
+                    required
+                    type="email" 
+                    value={email}
                 />
-            </form>
-        )
-    }
+            </label>
+            
+            <label>
+                password
+                <input 
+                    name="password"
+                    onChange={handleChange}
+                    placeholder="your password" 
+                    required
+                    type="password" 
+                    value={password}
+                />
+            </label>
 
+            <Button 
+                id="submit-sign-in" 
+                text="Submit"
+                type="submit"
+            />
+        </form>
+    )
 }
 
 export default SignIn
