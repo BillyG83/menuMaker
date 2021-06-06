2ee57a3d35462c0fd5591f4d4fb4fbed7f65ab48
BillyG83 <bgilmore83@hotmail.co.uk>
2021-06-06 11:17:00 +0200

    fix(page-title): fixed dumb error in function return

--- src/components/page-title/page-title.component.jsx
+++ src/components/page-title/page-title.component.jsx
@@ -1,9 +1,9 @@
 import React, { memo } from 'react'
 import './page-title.styles.scss'
 
-const PageTitle = memo(({ text }) => (
+const PageTitle = memo(({ text }) => {
     PageTitle.displayName = 'PageTitle'
-    (<h1 className="page-title">{ text }</h1>)
-))
+    return (<h1 className="page-title">{ text }</h1>)
+})
 
 export default PageTitle
