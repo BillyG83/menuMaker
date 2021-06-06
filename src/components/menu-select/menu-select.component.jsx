dce09b0ac0d192cbc09a99d8c535c0edbbbf1c38
BillyG83 <bgilmore83@hotmail.co.uk>
2021-06-06 12:22:13 +0200

    feat(landing-page): CSS of menu section and select to improve UX of landing page

--- src/components/menu-select/menu-select.component.jsx
+++ src/components/menu-select/menu-select.component.jsx
@@ -3,20 +3,25 @@ import './menu-select.styles.scss'
 
 const MenuSelect = memo(({ menuSections }) => {
 	MenuSelect.displayName = 'MenuSelect'
+
 	return(
 		<nav className="menu-select">
+			<ul>
 			{
 				menuSections && menuSections.map((menuSection, i) => {
 					return (
-						<a 
-							href={`#menu-section-${menuSection}`}
-							key={i}
-						>
-							{menuSection}
-						</a>
+						<li>
+							<a 
+								href={i === 0 ? '#root' : `#menu-section-${menuSection}`}
+								key={i}
+							>
+								{menuSection}
+							</a>
+						</li>
 					)
 				})
 			}
+			</ul>
 		</nav>
 	)
 })
