50cbfe6ef6e8d77cb1e582cf760083d4e7870e08
BillyG83 <bgilmore83@hotmail.co.uk>
2021-06-10 21:51:09 +0200

    feat(menu-page): started to build out menu edit features

--- src/pages/menu/menu.component.jsx
+++ src/pages/menu/menu.component.jsx
@@ -1,38 +1,72 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 import { selectCurrentUser } from '../../redux/user/user.selectors.js'
+import { selectMenuToEdit } from '../../redux/accounts/accounts.selectors.js'
 import { signInWithGoogle } from '../../firebase/firebase.js'
 import { auth } from '../../firebase/firebase.js'
 import PageTitle from '../../components/page-title/page-title.component.jsx'
+import EditMenu from '../../components/edit-menu/edit-menu.component'
 import Button from '../../components/button/button.component.jsx'
 import './menu.styles.scss'
 
-const MenuPage = ({ currentUser }) => (
-	<section className="menu-page">
-		<PageTitle text={'Update Menu'} />
-		<div className="page-inner">
-			<p>user can add, edit, remove menus</p>
+const MenuPage = ({ currentUser, accountToEdit }) => {
+	const hasAccountToEdit = Object.entries(accountToEdit)
+	
+	return (
+		<section className="menu-page">
+			<PageTitle text={'Update Menu'} />
+			<div className="page-inner">
+				{
+					hasAccountToEdit ?
+					<EditMenu />
+					:
+					<div>
+						<p>Oops there is no menu to edit</p>
+						<Link
+							className="button" 
+							to="/admin"
+						>
+							Admin Area
+						</Link>
+					</div>
+				}
+				{
+					currentUser ?
+						<div className="menu-page__footer">
+							<Button 
+								id="google-sign-in" 
+								text="Sign out"
+								clickEvent={auth.signOut()}
+							/>
+						</div>
+					:
+						<div className="menu-page__footer">
+							<p>Hey there, are you a customer?</p>
+							<p>If so use this button to sign in:</p>
+							<Button 
+								id="google-sign-in" 
+								text="Sign in with Google"
+								clickEvent={signInWithGoogle}
+							/>
+							<p>If not maybe you shouldn't be here..?</p>
+							<Link
+							 className="button" 
+							 to="/home"
+							>
+								Back our homepage
+							</Link>
+						</div>
+				}
+			</div>
+		</section>
+	)
+}
 
-			{
-				currentUser ?
-					<Button 
-						id="google-sign-in" 
-						text="Sign out"
-						clickEvent={auth.signOut()}
-					/>
-				:
-					<Button 
-						id="google-sign-in" 
-						text="Sign in with Google"
-						clickEvent={signInWithGoogle}
-					/>
-			}
-		</div>
-	</section>
-)
-
-const mapStateToProps = state => ({
-	currentUser: selectCurrentUser(state)
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+	accountToEdit: selectMenuToEdit
 })
 
 export default connect(mapStateToProps)(MenuPage)
