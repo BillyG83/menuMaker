60c6190b3cfe3c85629547429b25941688cf8722
BillyG83 <bgilmore83@hotmail.co.uk>
2021-09-08 17:29:23 +0200

    feat(Button): new colors, icons, width setting, click guard on icon & text

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,6 +1,6 @@
 import React, { memo, useContext } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faInfo, faLanguage, faShareAlt } from '@fortawesome/free-solid-svg-icons'
+import { faInfo, faLanguage, faShareAlt, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { ThemeContext } from '../../App.js'
 import './button.styles.scss'
 
@@ -11,9 +11,11 @@ const Button = memo((props) => {
     const {
         Id,
         clickEvent,
+        color,
         icon,
         text,
         type,
+        width,
     } = props
 
     const handleClick = (event) => {
@@ -23,12 +25,16 @@ const Button = memo((props) => {
 
     const getIcon = (icon) => {
         switch(icon) {
+            case 'add' :
+                return <FontAwesomeIcon icon={faPlus} />
             case 'share':
                 return <FontAwesomeIcon icon={faShareAlt} />
             case 'info':
                 return <FontAwesomeIcon icon={faInfo} />
             case 'language':
                 return <FontAwesomeIcon icon={faLanguage} />
+            case 'trash':
+                return <FontAwesomeIcon icon={faTrash} />
             default:
                 return
         }
@@ -36,22 +42,24 @@ const Button = memo((props) => {
 
     return(
         <button 
-        aria-label={text && text}
+            aria-label={text && text}
             className={`
                 button 
                 button--${theme} 
+                ${color ? `button--${color}` : ''}
+                ${width ? `button--${width}` : ''}
                 ${icon ? 'button--icon' : ''}
             `}
             id={`${Id}`}
             onClick={handleClick}
             type={type ? type : 'button'}
         >
+            <span className="no-click">
             {
-                icon && getIcon(icon)
+                icon ? getIcon(icon)
+                : props.children || text
             }
-            {
-                props.children || text
-            }
+            </span>
         </button>
     )
 })
