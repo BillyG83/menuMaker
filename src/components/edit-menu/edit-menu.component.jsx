2625aa768724c1ca6df3466db1661d1a690cdd85
BillyG83 <bgilmore83@hotmail.co.uk>
2021-09-09 18:40:34 +0200

    feat(EditMenu): improvements around adding and removing social links

--- src/components/edit-menu/edit-menu.component.jsx
+++ src/components/edit-menu/edit-menu.component.jsx
@@ -1,24 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { selectMenuToEdit } from '../../redux/accounts/accounts.selectors'
 import Button from '../button/button.component'
 import EditMenuSocial from '../edit-menu-social/edit-menu-social.component'
+import './edit-menu.styles.scss'
 
 const EditMenu = ({ menuData }) => {
     const [ businessCurrency, setBusinessCurrency ] = useState(menuData.businessCurrency)
     const [ businessName, setBusinessName ] = useState(menuData.businessName)
     const [ businessPostCode, setBusinessPostCode ] = useState(menuData.businessPostCode)
     const [ published, setPublished ] = useState(menuData.published)
-    const [ businessInfo, setBusinessInfo ]= useState(menuData.businessInfo)
-    const [ businessMenu, setBusinessMenu ]= useState(menuData.businessMenu)
-    const [ businessSocial, setBusinessSocial ]= useState(menuData.businessSocial)
+    const [ businessInfo, setBusinessInfo ] = useState(menuData.businessInfo)
+    const [ businessMenu, setBusinessMenu ] = useState(menuData.businessMenu)
+    const [ businessSocial, setBusinessSocial ] = useState(menuData.businessSocial)
+    const [ isDataUpdated, setIsDataUpdated ] = useState(false)
+
+    useEffect( () => {
+        console.log('businessSocial changed');
+    }, [ businessSocial ])
+
+    const updatesSaved = () => {
+        setIsDataUpdated(false)
+    }
 
     const handleClick = (event) => {
         console.log('businessSocial = ', businessSocial);
     }
 
     return(
-        <section>
+        <section className="edit-menu">
             <h3>{ businessName }</h3>
             <p>{ businessPostCode }</p>
             <p>{ businessCurrency }</p>
@@ -36,14 +46,24 @@ const EditMenu = ({ menuData }) => {
                 : null
             }
 
-            <Button 
-                clickEvent={handleClick}
-                color="green"
-            >
-                { published ? 'Update'
-                : 'Publish'
-                }
-            </Button>
+            <div className="edit-menu__buttons">
+                <Button 
+                    clickEvent={updatesSaved}
+                    disabled={isDataUpdated}
+                >
+                    Save Changes
+                </Button>
+
+                <Button 
+                    clickEvent={handleClick}
+                    color="green"
+                    disabled={published}
+                >
+                    { published ? 'Update'
+                    : 'Publish'
+                    }
+                </Button>
+            </div>
         </section>
     )
 }
