67add8c3dfe5fe9a85cad41cb741c3309fb1adbe
BillyG83 <bgilmore83@hotmail.co.uk>
2021-09-21 15:58:24 +0200

    feat(MenuItem): using List & IconList to render UI from JSON

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux'
 import { addItemToOrder } from '../../redux/cart/cart.actions.js'
 import Button from '../button/button.component.jsx'
 import ShowMoreLess from '../show-more-less/show-more-less.component.jsx'
+import IconList from '../icon-list/icon-list.component.jsx'
+import List from '../list/list.component.jsx'
 import './menu-item.styles.scss'
 
 const MenuItem = memo(({ data, addItemToOrder }) => {
@@ -10,36 +12,39 @@ const MenuItem = memo(({ data, addItemToOrder }) => {
 	
 	return(
 		<li className="menu-item">
-			<h4>{data.name}</h4>
+			<h3>{data.name}</h3>
 			<p>{data.detail}</p>
 			<strong className="price">{data.price}</strong>
 			
-			<Button 
-				Id="add-menu-item" 
-				text="Add to order"
-				clickEvent={() => { addItemToOrder(data) }}
-				icon="add"
-				color="green"
-			/>
-
+			<div className="menu-item__flex">
+				{
+					data.icons && data.icons.length > 0 ?
+					<IconList icons={data.icons} />
+					:
+					``
+				}
+				<Button 
+					Id="add-menu-item" 
+					text="Add to order"
+					clickEvent={() => { addItemToOrder(data) }}
+					icon="add"
+					color="green"
+				/>
+			</div>
+			
+			{
+				data.ingredients && data.ingredients.length > 0 ?
+				<List listItems={data.ingredients} listTitle="Ingredients" />
+				:
+				``
+			}
 			{
 				data.moreInfo && data.moreInfo.length > 0 ?
 				<ShowMoreLess title="More Info" content={data.moreInfo} />
 				:
 				``
 			}
-			{
-				data.icons && data.icons.length > 0 ?
-				<p>Icon Component</p>
-				:
-				``
-			}
-			{
-				data.ingredients && data.ingredients.length > 0 ?
-				<p>Ingredients (content component)</p>
-				:
-				``
-			}
+			
 		</li>
 	)
 })
