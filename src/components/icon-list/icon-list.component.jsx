bd79ab0b2a73ffa0bfaea38fe9a762623dbf5664
BillyG83 <bgilmore83@hotmail.co.uk>
2021-10-19 21:31:11 +0200

    feat(IconList): adding tooltip to icons

--- src/components/icon-list/icon-list.component.jsx
+++ src/components/icon-list/icon-list.component.jsx
@@ -30,12 +30,32 @@ const IconList = memo(({ icons }) => {
         }
     }
 
+    const handleClick = (event) => {
+        if (event.target.classList.contains('icon-list--open-tooltip')) {
+            event.target.classList.remove('icon-list--open-tooltip')
+        } else {
+            const iconListItems = document.querySelectorAll('.jsIconListItem')
+            iconListItems && iconListItems.forEach(item => item.classList.remove('icon-list--open-tooltip'))
+            event.target.classList.toggle('icon-list--open-tooltip')
+        }
+    }
+
     return (
         <ul className="icon-list">
             {
                 icons.map((icon, i) => {
                     return (
-                        <li key={`${icon.name}_${i}`}>{getIcon(icon.icon)}</li>
+                        <li 
+                            aria-label={`icon of ${icon.name}`} 
+                            key={`${icon.name}_${i}`}
+                            onClick={handleClick}
+                            className="jsIconListItem"
+                        >
+                            <span aria-hidden="true" className="icon-list__icon">{getIcon(icon.icon)}</span>
+                            <span aria-hidden="true" className="icon-list__tooltip">
+                                <span className="icon-list__tooltip-text">{icon.name}</span>
+                            </span>
+                        </li>
                     )
                 })
             }
