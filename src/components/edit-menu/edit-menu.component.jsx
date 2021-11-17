45d461a5fb82d3ebe0f88b8248537e9b444e7f85
BillyG83 <bgilmore83@hotmail.co.uk>
2021-11-17 09:48:07 +0100

    chore(EditMenu): passing currency

--- src/components/edit-menu/edit-menu.component.jsx
+++ src/components/edit-menu/edit-menu.component.jsx
@@ -63,6 +63,7 @@ const EditMenu = ({
             }</p>
 
             <MenuEditor 
+                businessCurrency={businessCurrency}
                 businessMenu={businessMenu}
                 setBusinessMenu={setBusinessMenu}
             />
